3c5bc35098f21b7a7817b9636187250e132245b1
Sören Glimm <sglimm@goodgamestudios.com>
2015-04-22 14:36:48 +0200

    Remove unused import

--- src/main/java/ws/wamp/jawampa/io/BaseClient.java
+++ src/main/java/ws/wamp/jawampa/io/BaseClient.java
@@ -3,8 +3,6 @@ package ws.wamp.jawampa.io;
 import ws.wamp.jawampa.WampClient;
 import ws.wamp.jawampa.messages.WampMessage;
 
-import com.fasterxml.jackson.databind.node.ArrayNode;
-
 /**
  * @author hkraemer@ggs-hh.net
  */
