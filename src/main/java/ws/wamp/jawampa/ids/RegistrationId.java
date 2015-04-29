2d95f94c2c8549e377518c00222ecbe8a2a07c43
Sören Glimm <sglimm@goodgamestudios.com>
2015-04-29 14:07:55 +0200

    Move common code between RegistrationId and RequestId to new BaseId

--- src/main/java/ws/wamp/jawampa/ids/RegistrationId.java
+++ src/main/java/ws/wamp/jawampa/ids/RegistrationId.java
@@ -1,44 +1,11 @@
 package ws.wamp.jawampa.ids;
 
-public class RegistrationId {
-    private final long value;
-
+public final class RegistrationId extends BaseId {
     private RegistrationId( long value ) {
-        this.value = value;
+        super( value );
     }
 
     public static RegistrationId of( long value ) {
         return new RegistrationId( value );
-    }
-
-    public long getValue() {
-        return value;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (int)( value ^ ( value >>> 32 ) );
-        return result;
-    }
-
-    @Override
-    public boolean equals( Object obj ) {
-        if ( this == obj )
-            return true;
-        if ( obj == null )
-            return false;
-        if ( getClass() != obj.getClass() )
-            return false;
-        RegistrationId other = (RegistrationId)obj;
-        if ( value != other.value )
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "RegistrationId [value=" + value + "]";
     }
 }
--- src/main/java/ws/wamp/jawampa/ids/RequestId.java
+++ src/main/java/ws/wamp/jawampa/ids/RequestId.java
@@ -1,44 +1,11 @@
 package ws.wamp.jawampa.ids;
 
-public class RequestId {
-    private final long value;
-
+public final class RequestId extends BaseId {
     private RequestId( long value ) {
-        this.value = value;
+        super( value );
     }
 
     public static RequestId of( long value ) {
         return new RequestId( value );
-    }
-
-    public long getValue() {
-        return value;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (int)( value ^ ( value >>> 32 ) );
-        return result;
-    }
-
-    @Override
-    public boolean equals( Object obj ) {
-        if ( this == obj )
-            return true;
-        if ( obj == null )
-            return false;
-        if ( getClass() != obj.getClass() )
-            return false;
-        RequestId other = (RequestId)obj;
-        if ( value != other.value )
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "RequestId [value=" + value + "]";
     }
 }
