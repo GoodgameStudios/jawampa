c1077a6296f613eae9030b20c486b867b541a59b
Sören Glimm <sglimm@goodgamestudios.com>
2015-05-06 21:10:17 +0200

    Serialization of AbortMessage was missing the message code

--- src/main/java/ws/wamp/jawampa/messages/AbortMessage.java
+++ src/main/java/ws/wamp/jawampa/messages/AbortMessage.java
@@ -25,6 +25,7 @@ public class AbortMessage extends WampMessage {
 
     public JsonNode toObjectArray(ObjectMapper mapper) throws WampError {
         ArrayNode messageNode = mapper.createArrayNode();
+        messageNode.add(ID);
         if (details != null)
             messageNode.add(details);
         else
